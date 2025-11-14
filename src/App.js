5b4008cdb9e8eac7f41a30241f0342b8948b85e9
abdou aziz kan <kane.aziz@uam.edu.sn>
2025-11-14 10:59:24 +0000

    ajout d'un projet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Portfolio from './components/Portfolio';
 import TypingGame from './components/typing-game';
+import ChiffrementDes from './components/chiffrement-des';
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
         
         {/* Sites externes/projets */}
         <Route path="/typing-game" element={<TypingGame />} />
+        <Route path="/chiffrement-des" element={<ChiffrementDes />} />
       </Routes>
     </BrowserRouter>
   );
